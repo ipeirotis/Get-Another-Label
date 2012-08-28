be3bf2749ad9da4dd17e3c19618d12b80072c32c
Aldrin Leal <aldrin@leal.eng.br>
2012-08-28 08:43:38 -0300

    Added a more clear EngineTest, encompassing CLI Parsing

--- src/test/java/com/ipeirotis/gal/EngineTest.java
+++ src/test/java/com/ipeirotis/gal/EngineTest.java
@@ -1,7 +1,10 @@
 package com.ipeirotis.gal;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.kohsuke.args4j.CmdLineParser;
 
 import com.ipeirotis.gal.engine.Engine;
 import com.ipeirotis.gal.engine.EngineContext;
@@ -9,15 +12,22 @@ import com.ipeirotis.gal.engine.EngineContext;
 public class EngineTest {
 	private EngineContext ctx;
 
+	private CmdLineParser parser;
+
 	@Before
 	public void before() {
 		ctx = new EngineContext();
+
+		parser = new CmdLineParser(ctx);
 	}
 
 	@Test
 	public void testHappyPath() throws Exception {
-		ctx.setInputFile("data/test-unlabeled.txt");
-		ctx.setCategoriesFile("data/test-categories.txt");
+		parser.parseArgument("--input data/test-unlabeled.txt --categories data/test-categories.txt --dry-run"
+				.split("\\s+"));
+
+		assertEquals(ctx.getInputFile(), "data/test-unlabeled.txt");
+		assertEquals(ctx.getCategoriesFile(), "data/test-categories.txt");
 		
 		Engine engine = new Engine(ctx);
 		
