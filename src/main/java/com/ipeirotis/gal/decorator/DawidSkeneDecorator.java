8817628fd3adea989992fad30c502be619bb691a
Aldrin Leal <aldrin@leal.eng.br>
2012-10-05 18:14:22 -0300

    Support for #32

--- src/main/java/com/ipeirotis/gal/decorator/DawidSkeneDecorator.java
+++ src/main/java/com/ipeirotis/gal/decorator/DawidSkeneDecorator.java
@@ -12,27 +12,59 @@ public class DawidSkeneDecorator extends Decorator<DawidSkene> {
 		super(wrapped);
 	}
 
-	public ConfusionMatrix getConfusionMatrix(ClassificationMethod clasMethod) {
-		ConfusionMatrix confMatrix = new ConfusionMatrix(object.getCategories().values());
-		
+	public ConfusionMatrix getEstimatedConfusionMatrix(
+			ClassificationMethod clasMethod) {
+		ConfusionMatrix confMatrix = new ConfusionMatrix(object.getCategories()
+				.values());
+
 		confMatrix.empty();
-		
+
+		for (Datum d : object.getObjects().values()) {
+			for (String fromCategory : d.getProbabilityVector(clasMethod)
+					.keySet()) {
+
+				Double fromProb = d.getCategoryProbability(clasMethod, fromCategory);
+
+				Map<String, Double> toClassifiedAs = d
+						.getProbabilityVector(clasMethod);
+
+				for (String toCategory : toClassifiedAs.keySet()) {
+					Double toProb = toClassifiedAs.get(toCategory);
+
+					confMatrix.addError(fromCategory, toCategory, toProb
+							* fromProb);
+				}
+			}
+		}
+
+		confMatrix.normalize();
+
+		return confMatrix;
+	}
+
+	public ConfusionMatrix getConfusionMatrix(ClassificationMethod clasMethod) {
+		ConfusionMatrix confMatrix = new ConfusionMatrix(object.getCategories()
+				.values());
+
+		confMatrix.empty();
+
 		for (Datum d : object.getObjects().values()) {
 			if (!d.isEvaluation())
 				continue;
-			
+
 			String fromCategory = d.getEvaluationCategory();
-			Map<String, Double> toClassifiedAs = d.getProbabilityVector(clasMethod);
-			
+			Map<String, Double> toClassifiedAs = d
+					.getProbabilityVector(clasMethod);
+
 			for (String toCategory : toClassifiedAs.keySet()) {
 				Double prob = toClassifiedAs.get(toCategory);
-				
+
 				confMatrix.addError(fromCategory, toCategory, prob);
 			}
 		}
-		
+
 		confMatrix.normalize();
-		
+
 		return confMatrix;
 	}
 
--- src/main/java/com/ipeirotis/gal/engine/rpt/ConfusionMatrixReport.java
+++ src/main/java/com/ipeirotis/gal/engine/rpt/ConfusionMatrixReport.java
@@ -1,6 +1,8 @@
 package com.ipeirotis.gal.engine.rpt;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import com.ipeirotis.gal.decorator.DawidSkeneDecorator;
 import com.ipeirotis.gal.scripts.ConfusionMatrix;
@@ -30,11 +32,34 @@ public class ConfusionMatrixReport extends Report {
 		DawidSkene ds = ctx.getDawidSkene();
 		DawidSkeneDecorator decorator = new DawidSkeneDecorator(ds);
 		
+		String type = "Estimated";
+		
+		List<ClassificationMethod> estimatedClasMethods = Arrays.asList(ClassificationMethod.DS_Soft, ClassificationMethod.MV_Soft);
+		
+		for (ClassificationMethod estimatedClasMethod : estimatedClasMethods) {
+			ConfusionMatrix confMatrix = decorator.getEstimatedConfusionMatrix(estimatedClasMethod);
+			
+			reportTarget.println("%s Confusion Matrix (%s):", type, estimatedClasMethod.name());
+			
+			for (String from : confMatrix.getCategoryNames()) {
+				for (String to : confMatrix.getCategoryNames()) {
+					Double cm_entry = confMatrix.getErrorRate(from, to);
+					String s_cm_entry = Double.isNaN(cm_entry) ? "---" : Utils
+							.round(100 * cm_entry, 3).toString();
+					reportTarget.print("P[%s->%s]=%s\t", from, to, s_cm_entry);
+				}
+				reportTarget.println("");
+			}
+			reportTarget.println("");
+		}
+
+		type = "Evaluation";
+		
 		// for each classification method, we need to create a confusion matrix
 		for (ClassificationMethod clasMethod : Datum.ClassificationMethod.values()) {
 			ConfusionMatrix confMatrix = decorator.getConfusionMatrix(clasMethod);
 			
-			reportTarget.println("Confusion Matrix (%s):", clasMethod.name());
+			reportTarget.println("%s Confusion Matrix (%s):", type, clasMethod.name());
 			
 			for (String from : confMatrix.getCategoryNames()) {
 				for (String to : confMatrix.getCategoryNames()) {
